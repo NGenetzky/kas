8f0b4ed4f72d2166bc13df2596769e8cab17a13f
Nathan Genetzky <nathan@genetzky.us>
2020-07-25 14:33:15 -0500

    [WIP] tests: Start modifying repos_fetch

--- tests/test_repos_fetch.py
+++ tests/test_repos_fetch.py
@@ -1,31 +1,21 @@
-# kas - setup tool for bitbake based projects
-#
-# Copyright (c) Siemens AG, 2019
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-
 import os
 import shutil
 from kas import kas
 from kas.libkas import run_cmd
 
 import pytest
+
+KAS_CONFIG = {
+    'test.yml': """\
+header:
+  version: 8
+repos:
+  this:
+  kas:
+    url: https://github.com/siemens/kas.git
+    refspec: 907816a5c4094b59a36aec12226e71c461c05b77
+""",
+}
 
 
 @pytest.fixture
@@ -34,14 +24,14 @@ def changedir():
     os.chdir(os.path.join(os.path.dirname(__file__), '..'))
 
 
-def test_refspec_switch(changedir, tmpdir):
+def test_repos_fetch(changedir, tmpdir):
     """
         Test that the local git clone is correctly updated when switching
         between a commit hash refspec and a branch refspec.
     """
-    tdir = str(tmpdir.mkdir('test_refspec_switch'))
+    tdir = str(tmpdir.mkdir('test_repos_fetch'))
     shutil.rmtree(tdir, ignore_errors=True)
-    shutil.copytree('tests/test_refspec', tdir)
+    # shutil.copytree('tests/test_refspec', tdir) # TODO create "{tdir}/test.yml"
     os.chdir(tdir)
 
     kas.kas(['shell', 'test.yml', '-c', 'true'])
@@ -52,22 +42,19 @@ def test_refspec_switch(changedir, tmpdir):
     (rc, output) = run_cmd(['git', 'rev-parse', '-q', 'HEAD'], cwd='kas',
                            fail=False, liveupdate=False)
     assert rc == 0
-    assert output.strip() == '907816a5c4094b59a36aec12226e71c461c05b77'
-    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas2',
-                           fail=False, liveupdate=False)
-    assert rc == 0
     assert output.strip() == 'refs/heads/master'
 
-    kas.kas(['shell', 'test2.yml', '-c', 'true'])
+    # TODO: Add more git remotes
+
+    kas.kas(['shell', 'test.yml', '-c', 'true'])
     (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas',
                            fail=False, liveupdate=False)
-    assert rc == 0
-    assert output.strip() == 'refs/heads/master'
-    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas2',
+    kas.kas(['shell', 'test.yml', '-c', 'true'])
+    (rc, output) = run_cmd(['git', 'symbolic-ref', '-q', 'HEAD'], cwd='kas',
                            fail=False, liveupdate=False)
     assert rc != 0
     assert output.strip() == ''
-    (rc, output) = run_cmd(['git', 'rev-parse', '-q', 'HEAD'], cwd='kas2',
+    (rc, output) = run_cmd(['git', 'rev-parse', '-q', 'HEAD'], cwd='kas',
                            fail=False, liveupdate=False)
     assert rc == 0
-    assert output.strip() == '907816a5c4094b59a36aec12226e71c461c05b77'
+    assert output.strip() == 'refs/heads/master'
